893dde50e99c46ce6e84ca8a91b07fab7b8dd7e6
Chris Sheppard <github@chrissheppard.co.uk>
2014-11-05 12:37:16 +0000

    Switching to gulp

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+var DIST = "dist";
+
+var gulp = require("gulp");
+
+var del = require("del");
+var browserSync = require("browser-sync");
+var runSequence = require("run-sequence");
+
+// load plugins
+var $ = require("gulp-load-plugins")();
+
+
+gulp.task("css", function () {
+    return gulp.src("src/assets/scss/*.scss")
+        .pipe($.cached("css"))
+        .pipe($.sass({
+            style: "expanded"
+        }))
+        .pipe($.autoprefixer("last 2 version", "safari 5", "ie 9", "opera 12.1", "ios 6", "android 4"))
+        .pipe(gulp.dest(DIST + "/assets/css"))
+        .pipe($.notify({message: "CSS task complete", onLast: true}))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task("html", function () {
+    return gulp.src("src/views/pages/**/*.hbs")
+        .pipe($.cached("html"))
+
+        .pipe($.assemble({
+            layoutdir: "src/views/layouts/",
+            layout: "default.hbs",
+            partials: [
+                "src/views/partials/**/*.hbs",
+                "src/views/components/**/*.hbs"
+            ],
+            data: "src/views/data/**/*.json",
+            assets: DIST
+        }))
+
+        .pipe($.inject(gulp.src("assets/css/**/*.css", {
+            read: false,
+            cwd: DIST
+        }), {addRootSlash: false}))
+
+        .pipe(gulp.dest(DIST))
+        .pipe($.notify({message: "HTML task complete", onLast: true}));
+});
+
+
+gulp.task("fonts", function () {
+    return gulp.src("src/assets/font/**/*.*")
+        .pipe(gulp.dest(DIST + "/assets/font/"))
+});
+
+
+gulp.task("clean", function (cb) {
+    del([
+        DIST + "/assets",
+        DIST + "/**/*.html"
+    ], cb)
+});
+
+
+// start server
+gulp.task("browser-sync", function () {
+    browserSync({
+        server: {
+            baseDir: DIST,
+            directory: true,
+            index: "index.html"
+        },
+        startPath: "/index.html"
+    });
+});
+
+
+// Reload all Browsers
+gulp.task("bs-reload", function () {
+    browserSync.reload();
+});
+
+
+gulp.task("watch", ["browser-sync"], function () {
+    // Watch sass files
+    gulp.watch("src/assets/scss/**/*.scss", ["css"]);
+
+    // Watch html files
+    gulp.watch("src/views/**/*.html", ["html"]);
+
+    // Reloading
+    gulp.watch("dist/**/*.html", ["bs-reload"]);
+});
+
+
+gulp.task("build", function (callback) {
+    runSequence(
+        "clean",
+        ["css", "fonts"],
+        "html",
+        callback
+    );
+});
+
+gulp.task("default", ["build"]);
